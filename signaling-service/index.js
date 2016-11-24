50bc5d99b2d23ff114ca93d688e91acbc734ec19
wangpin <pin.wang@hp.com>
2016-11-24 19:28:57 +0800

    Update main program

--- signaling-service/index.js
+++ signaling-service/index.js
@@ -10,6 +10,8 @@ var app = http.createServer(function(req, res) {
   fileServer.serve(req, res);
 }).listen(8080);
 
+var rooms = {};
+
 var io = socketIO.listen(app);
 io.sockets.on('connection', function(socket) {
 
@@ -30,23 +32,53 @@ io.sockets.on('connection', function(socket) {
     log('Received request to create or join room ' + room);
 
     var numClients = io.sockets.sockets.length;
+
     log('Room ' + room + ' now has ' + numClients + ' client(s)');
 
     if (numClients === 1) {
+      rooms[room] = { creator: {}};
       socket.join(room);
-      log('Client ID ' + socket.id + ' created room ' + room);
+      log('Client ID ' + socket.id + ' created room ' + room, JSON.stringify(rooms[room]));
       socket.emit('created', room, socket.id);
-
-  } else if (numClients === 2) {v
+  } else if (numClients === 2) {
       log('Client ID ' + socket.id + ' joined room ' + room);
       io.sockets.in(room).emit('join', room);
       socket.join(room);
       socket.emit('joined', room, socket.id);
       io.sockets.in(room).emit('ready');
+      io.sockets.in(room).emit('about creator', rooms[room].creator, room);
+      rooms[room].participant = {};
     } else { // max two clients
       socket.emit('full', room);
     }
   });
+
+  socket.on('candidate from creator', function(candidate, room){
+      log('Receive candidate of creator:n' + candidate + ' room ' + room);
+      rooms[room].creator.candidate = candidate;
+  });
+
+  socket.on('desc from creator', function(desc, room){
+      log('Receive desc of creator:\n' + desc + ' room ' + room);
+      rooms[room].creator.desc = desc;
+  })
+
+  socket.on('candidate from participant', function(candidate, room){
+      rooms[room].participant.candidate = candidate;
+      log('Receive candidate of participant:n' + candidate + ' room ' + room);
+      if(rooms[room].participant.candidate &&　rooms[room].participant.desc){
+          io.sockets.in(room).emit('about participant', rooms[room].participant, room);
+      }
+  });
+
+  socket.on('desc from participant', function(candidate, room){
+      rooms[room].participant.desc = desc;
+      log('Receive candidate of participant:n' + candidate + ' room ' + room);
+      if(rooms[room].participant.candidate &&　rooms[room].participant.desc){
+          io.sockets.in(room).emit('about participant', rooms[room].participant, room);
+      }
+  });
+
 
   socket.on('ipaddr', function() {
     var ifaces = os.networkInterfaces();
--- signaling-service/js/main.js
+++ signaling-service/js/main.js
@@ -13,6 +13,7 @@ if (room !== "") {
 
 socket.on('created', function(room, clientId) {
   isInitiator = true;
+  createConnection();
 });
 
 socket.on('full', function(room) {
@@ -25,8 +26,77 @@ socket.on('ipaddr', function(ipaddr) {
 
 socket.on('joined', function(room, clientId) {
   isInitiator = false;
+  createConnection();
 });
 
 socket.on('log', function(array) {
   console.log.apply(console, array);
 });
+
+var sendChannel = null;
+
+function createConnection(){
+    var servers = null,
+    pcConstraint = null,
+    dataConstraint = null,
+    localConnection = null,
+    dataChannel = null;
+
+    trace('Using SCTP based data channels');
+    localConnection = new RTCPeerConnection(servers, pcConstraint);
+    localConnection.onicecandidate = function(event){
+        if(event.candidate){
+            if(isInitiator){
+                socket.emit('candidate from creator', event.candidate, room);
+                socket.on('about participant', function(data, room){
+                    var candidate = data.candidate,
+                        desc = data.desc;
+
+                    localConnection.addIceCandidate(new RTCIceCandidate(candidate));
+                    localConnection.setRemoteDescription(desc);
+                });
+
+            }else{
+                socket.emit('candidate from participant', event.candidate, room);
+            }
+        }
+    };
+
+
+    if(isInitiator){
+        localConnection.createOffer().then(function(desc){
+            localConnection.setLocalDescription(desc).then(
+              function() {
+                 socket.emit('desc from creator', desc, room);
+                 trace('LocalConnection setLocalDescription complete');
+              },
+              function() {
+                 trace('LocalConnection setLocalDescription failed');
+              }
+            );
+        });
+    }else{
+
+        socket.on('about creator', function(data, room){
+            var candidate = data.candidate,
+                desc = data.desc;
+
+            localConnection.addIceCandidate(new RTCIceCandidate(candidate));
+            localConnection.setRemoteDescription(desc);
+
+            localConnection.createAnswer().then(function(desc){
+                localConnection.setLocalDescription(desc);
+                socket.emit('desc from participant', desc, room);
+            });
+        });
+    }
+
+
+
+    trace('Created local peer connection object localConnection');
+    dataChannel = localConnection.createDataChannel('dataChannel',
+        dataConstraint);
+    trace('Created data channel');
+
+
+}
